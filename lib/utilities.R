6e9140fc61b2fcd22b2b7d5cc1c7f9a8d2dd09a6
Ryan Triolo <rtriolo@gmail.com>
2021-06-16 09:15:43 -0700

    Config and utilities updates

--- lib/utilities.R
+++ lib/utilities.R
@@ -18,25 +18,33 @@ drawFolds <- function(n, K=5) {
   return(test_ids)
 }
 
+# Build formula string for models
+buildFormula <- function(dep_var, covariates) {
+  f <- paste0(covariates, collapse = " + ")
+  f <- paste0(dep_var, " ~ ", f)
+  return(as.formula(f))
+}
+
 # Run boosted tree for all shrinkage and depth values
-tuneBoostedTree <- function(data, shrinkage_vals, depth_vals, num_trees) {
-  grid <- shrinkage_vals
+tuneBoostedTree <- function(data, dep_var, covariates, shrinkage_vals, depth_vals, num_trees, test_ids) {
+  
   result <- tibble()
+  f <- paste0("log(", dep_var, ")") %>% buildFormula(covariates)
   
   for (d in depth_vals) {
     cat(paste0("Depth: ",d,"\n"))
-    for (g in grid) {
+    for (g in shrinkage_vals) {
       cat(paste0("  Shrinkage: ",g,"\n"))
       sq.error <- c()
       for (k in 1:5) {
         cat(paste0("    Fold: ",k,"\n"))
         test_ix <- test_ids[[k]]
-        boost.uplift <- gbm(log(CC6620) ~ . - Date - Year - CC6630 - CC66200, 
+        boost.uplift <- gbm(f, 
                             data = data[-test_ix,], 
                             distribution = "gaussian", n.trees = num_trees, interaction.depth = d, 
                             shrinkage = g)
-        y.hat.boost <- predict(boost.uplift, newdata = data[test_ix,], n.trees = num_trees)
-        uplift.test <- data$CC6620[test_ix] %>% log
+        y.hat.boost <- boost.uplift %>% predict(newdata = data[test_ix,], n.trees = num_trees)
+        uplift.test <- data[test_ix, dep_var] %>% log %>% unlist
         sq.error <- c(sq.error, (y.hat.boost - uplift.test)^2)
       }
       result_i <- tibble(
@@ -50,4 +58,77 @@ tuneBoostedTree <- function(data, shrinkage_vals, depth_vals, num_trees) {
   
   return(result)
   
+}
+
+# Function to fit cross-fit GBM
+cf_gbm <- function(data, model_f, folds, min.depth, min.shrinkage, num_trees) {
+  
+  K <- length(folds)
+  y.hat.full <- c()
+  
+  for (k in 1:K) {
+    cat(paste0("  Fold ",k,"\n"))
+    test_ix <- folds[[k]]
+    boost.uplift <- gbm(model_f,
+                        data = data[-test_ix,],
+                        distribution = "gaussian", n.trees = num_trees,
+                        interaction.depth = min.depth,
+                        shrinkage = min.shrinkage)
+    y.hat.boost <- predict(boost.uplift, newdata = data[test_ix,], n.trees = num_trees)
+    y.hat.full <- c(y.hat.full, y.hat.boost)
+  }
+  
+  y.hat.ordered <- rep(NA, nrow(data))
+  y.hat.ordered[unlist(folds)] <- y.hat.full
+  return(y.hat.ordered)
+}
+
+# Function to perform backward stepwise selection algorithm
+backwardStepwiseSelection <- function(data, dep_var, covariates, test_ids, min_depth, min_shrinkage, 
+                                      selection_threshold, num_trees) {
+  max_iter <- length(covariates)
+  
+  # Fit full model
+  model_f <- paste0("log(", dep_var, ")") %>% buildFormula(covariates)
+  y_hat <- data %>% cf_gbm(model_f, test_ids, min_depth, min_shrinkage, num_trees)
+  mse_min <- mean((y_hat - unlist(log(data[dep_var])))^2)
+  
+  # Save result to tibble
+  result_table <- tibble(
+    Vars.Dropped = 0,
+    Var.Name = "Base",
+    MSE = mse_min
+  )
+  
+  # Start backward stepwise selection loop
+  iter <- 1
+  while(iter <= max_iter) {
+    mse_c <- c()
+    for (c in covariates) {
+      cat(paste0("Drop var: ",c,"\n"))
+      covariates_i <- covariates[covariates != c]
+      model_f <- paste0("log(", dep_var, ")") %>% buildFormula(covariates_i)
+      y_hat <- data %>% cf_gbm(model_f, test_ids, min_depth, min_shrinkage, num_trees)
+      mse_c <- c(mse_c, mean((y_hat - unlist(log(data[dep_var])))^2))
+    }
+
+    if (min(mse_c - mse_min) > selection_threshold) { break() }
+
+    drop_var <- covariates[which.min(mse_c)]
+    covariates <- covariates[covariates != drop_var]
+
+    result_table_i <- tibble(
+      Vars.Dropped = iter,
+      Var.Name = drop_var,
+      MSE = min(mse_c)
+    )
+
+    result_table <- result_table %>% bind_rows(result_table_i)
+    mse_min <- min(mse_c)
+    iter <- iter + 1
+  }
+  
+  result <- list(result_table = result_table, covariates = covariates)
+  
+  return(result)
 }
---
+++ uplift_model.R
@@ -0,0 +1,80 @@
+
+library(gbm)
+library(glmnet)
+library(lmtest)
+library(lubridate)
+library(rjson)
+library(sandwich)
+library(tidyverse)
+
+# Load settings, data, and utility functions
+settings <- fromJSON(file = "config.json")
+load("data/model_data_completeObs.RData")
+source("lib/utilities.R")
+
+# Create settings variables
+set.seed(settings$random_seed)
+n <- nrow(model_data)
+num_folds <- settings$num_folds
+shrinkage_vals <- seq(from = settings$boost_tuning$shrinkage_vals$from, 
+                      by = settings$boost_tuning$shrinkage_vals$by, 
+                      length.out = settings$boost_tuning$shrinkage_vals$length.out)
+depth_vals <- (settings$boost_tuning$depth_vals$min):(settings$boost_tuning$depth_vals$max)
+num_trees <- settings$boost_tuning$num_trees
+dep_var <- settings$y_var
+covariates <- colnames(model_data)
+covariates <- covariates[!(covariates %in% settings$exclude_variables)]
+min_depth <- settings$boost_min_depth
+min_shrinkage <- settings$boost_min_shrinkage
+selection_threshold <- settings$boost_backward_selection$exit_threshold
+
+# Draw folds for cross-fitting
+test_folds <- drawFolds(n, K = num_folds)
+
+########### TUNING OF BOOSTED REGRESSION TREE #############
+if (settings$boost_tuning$run_boost_tuning) {
+  tuning_result <- model_data %>% 
+    tuneBoostedTree(dep_var, covariates, shrinkage_vals, depth_vals, num_trees, test_folds)
+  save(tuning_result, settings, file = "output/tuning_result.RData")
+}
+
+########### BACKWARD STEPWISE BOOSTED REGRESSION TREE ###########
+if (settings$boost_backward_selection$run_backward_stepwise) {
+  selection_result <- model_data %>% 
+    backwardStepwiseSelection(dep_var, covariates, test_folds, min_depth, min_shrinkage, selection_threshold,
+                              num_trees)
+  save(selection_result, settings, file = "output/selection_result.RData")
+}
+
+########### BEGIN: FIT CROSS-FIT REDUCED MODEL ###########
+# 
+# # Fit reduced model
+# cat(paste0("Running:\n"))
+# model_f <- buildFormula(dep_var, covariates)
+# y_hat <- cf_gbm(model_f, model_data, test_ids, min.depth, min.shrinkage)
+# 
+# tibble(
+#   Type = c(rep("Predicted", 727), rep("Actual", 727)) %>% as.factor,
+#   Index = c(1:727, 1:727),
+#   Value = c(y_hat, log(model_data$CC6620))
+#   ) %>%
+#     ggplot(aes(x = Index, y = Value, color = Type)) +
+#       geom_line()
+# 
+# save(y_hat, file = "boosting_results/yhat_reduced_CF_boost.RData")
+# 
+# ########### END: FIT CROSS-FIT REDUCED MODEL ###########
+# 
+# ########### BEGIN: FIT FULL MODEL ###########
+# ## Fit full dataset, to view variable importance
+# boost.uplift.full <- gbm(model_f, 
+#                       data = model_data, 
+#                       distribution = "gaussian", n.trees = 5000, 
+#                       interaction.depth = min.depth, 
+#                       shrinkage = min.shrinkage)
+# save(boost.uplift.full, file="boosting_results/boost_uplift_noCF.RData")
+# 
+# plot(boost.uplift.full, i="Load.Mileage.MW")
+# 
+# ########### END: FIT FULL MODEL ###########
+# 
